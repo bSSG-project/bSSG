4692c55769d7d491cdba5b09e8b93baacc306443
Bevan0 <bevanellis8@gmail.com>
2022-09-30 11:29:20 +1300

    commiting this here so I can work on file watching while away

---
+++ bssgwatch.py
@@ -0,0 +1,10 @@
+# NOTE TO SELF
+# THIS NEEDS TO DETECT A FILE CREATION/EDIT/DELETION IN templates/ AND content/
+# IF ONE IS DETECTED, RUN THE COMMAND 'bssg-generate'
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()

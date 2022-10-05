19ed864a3238a910b4295abf2ab10d9904e9be82
Bevan0 <bevanellis8@gmail.com>
2022-10-05 13:23:49 +1300

    trying this

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 setup(
     name="bSSG",
     version="0.1.0",
-    package_dir={"bSSG": "src"},
+    package_dir={"bssg": "src"},
     install_requires=[
         "markdown",
         "watchdog"
     ],
     entry_points={
         "console_scripts": [
-            'bssg-generate = bSSG.bssggenerate:main',
-            'bssg-watch = bSSG.bssgwatch:main'
+            'bssg-generate = bssg.bssggenerate:main',
+            'bssg-watch = bssg.bssgwatch:main'
         ]
     }
 )

2244b2a7c111027192ab5563f8668018a42d4c8c
Bevan0 <bevanellis8@gmail.com>
2022-11-09 19:59:23 +1300

    is setup.py gonna work this time?

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="bSSG",
     version="0.1.0",
-    package_dir={"": "bSSG"},
     install_requires=[
         "markdown",
         "watchdog"
     ],
+    packages=['bSSG'],
     entry_points={
         "console_scripts": [
             'bssg-generate = bssggenerate:main',
